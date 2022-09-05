d7c8070d05b9f5fc12b5f617eb33c3300ffb23e3
aluno <aluno@LAB.ETEC187>
2022-09-05 19:54:57 -0300

    Projeto inicial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Projeto meus filmes 👌👌👌👌👌</Text>
       
       <StatusBar style="auto" />
     </View>
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+

a22fa599c61dfc033a720bf395168778582fb8b9
lucas <lucasfanjos0022@gmail.com>
2022-09-26 21:17:35 -0300

    marcos quase vomitou"

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,14 +1,58 @@
-import react from "react";
+import react, {useState} from "react";
 
-import {View, Text, Input, TouchableOpacity, StyleSheet, TextInput} from 'react-native';
+import {View, Text, Input, TouchableOpacity, StyleSheet, TextInput, FlatList} from 'react-native';
 
 
 export default function Home(){
+
+
+    let[listaFilmes, setLista] = useState([
+        {
+            'id':1,
+            'nome': 'João Amorim: Obrigado a matar'
+        },
+
+        {
+            'id':2,
+            'nome': 'Interstellar'
+        },
+
+        {
+            'id':2,
+            'nome': 'Interstellar'
+        },
+
+        {
+            'id':2,
+            'nome': 'Interstellar'
+        },
+
+        {
+            'id':2,
+            'nome': 'Interstellar'
+        },
+
+        {
+            'id':2,
+            'nome': 'Interstellar'
+        },
+
+
+        {
+            'id':3,
+            'nome': 'A Saga Crepúsculo: Amanhecer - Parte II'
+        },
+        
+        
+    ]);
+
+    
+
     return(
         <View style = {styles.container}>
             <View style = {styles.bodyBox}>
-                <Text style = {styles.titulo}>🎬Meus filmes🎬</Text>
-                <Text style = {styles.subtitulo}>Boa noite, Luquinhas... </Text>
+                <Text style = {styles.titulo}>Meus filmes 🎬</Text>
+                <Text style = {styles.subtitulo}>Boa noite, usuário</Text>
 
                 <TextInput style = {styles.input} placeholder = "Digite seu filme...">
 
@@ -19,8 +63,17 @@ export default function Home(){
 
             </View>
 
-            <View style = {styles.bodyBox}>
-            <Text style = {styles.titulo}>Nome do filme:</Text>
+            <View style = {styles.bodyBoxMovies}>
+                <FlatList
+                    data  ={listaFilmes}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) =>(
+                    <TouchableOpacity style ={styles.movieButton}>
+                         <Text style = {styles.tituloMovie}>{item.nome}</Text>
+                    </TouchableOpacity>
+                    )}
+                />
+            
             </View>
             
 
@@ -37,36 +90,55 @@ const styles = StyleSheet.create({
     },
 
     bodyBox: {
-        width: '400px',
-        alignItems: 'center',
-        padding:'50px',
+        width: '300px',
+        alignItems: 'left',
+        padding:'25px',
         backgroundColor:'#2a3042',
         borderRadius:'20px',
         shadowColor: '#000',
         shadowOffset: {width: 0, height: 4},
         shadowOpacity: 0.5,
         shadowRadius: 30,
-        margin:'20px',
-    },
+        margin:'10px',
+        },
+        
+        bodyBoxMovies: {
+            width: '300px',
+            alignItems: 'left',
+            padding:'15px',
+            backgroundColor:'#2a3042',
+            borderRadius:'20px',
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: 4},
+            shadowOpacity: 0.5,
+            shadowRadius: 30,
+            margin:'10px',
+            },
 
     titulo: {
-        fontSize: '35px',
+        fontSize: '22px',
+        color: '#fff',
+        fontWeight: 'bold',
+        
+    },
+
+    tituloMovie: {
+        fontSize: '18px',
         color: '#fff',
         fontWeight: 'bold',
         
     },
 
     subtitulo: {
-        fontSize: '15px',
+        fontSize: '10px',
         color: '#fff',
     },
 
     botao:{
         backgroundColor:'#eb38a3',
-        paddingHorizontal:'30px',
+        paddingHorizontal:'20px',
         paddingVertical:'10px',
         borderRadius:'20px',
-        margin:'15px',
         marginTop:'10px',
         shadowColor: '#000',
         shadowOffset: {width: 0, height: 4},
@@ -74,27 +146,38 @@ const styles = StyleSheet.create({
         shadowRadius: 10,
     },
 
-    image: {
-        flex: 1,
-        justifyContent: "center"
-      },
+
 
       textoBotao: {
         color:'#000',
-        fontSize: '20px',
+        fontSize: '15px',
         color: '#fff',
         fontWeight: 'bold',
     },
 
     input:{
         backgroundColor:'#13131a',
-        padding:'8px',
-        margin:'15px',
-        paddingHorizontal: '50px',
+        padding:'5px',
+        marginVertical:'15px',
+        width:'250px',
+        paddingHorizontal: '10px',
         borderRadius:'5px',
-        fontSize: '20px',
+        fontSize: '18px',
         color:'#fff',
         fontWeight: 'bold',
+    },
+
+    movieButton:{
+        paddingHorizontal: '10px',
+        backgroundColor:'#181b25',
+        padding:'5px',
+        paddingVertical:'10px',
+        margin:'5px',
+        borderRadius:'12px',
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.6,
+        shadowRadius: 2,
     }
     
   });
